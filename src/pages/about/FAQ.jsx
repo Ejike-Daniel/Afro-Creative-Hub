98336b2759bbb3c7631b55046c8a3207eed0b6a0
Elrey009 <daniloejike@gmail.com>
2024-05-17 08:59:32 +0100

    modified about page

--- src/pages/about/FAQ.jsx
+++ src/pages/about/FAQ.jsx
@@ -1,12 +1,15 @@
-import car from '../../assets/car-img.png'
+import car from "../../assets/car-img.png";
+import Accordion from "./Accordion";
 
 const FAQ = () => {
   return (
-    <section className= ' shadow-inner shadow-smokeyGrey'>
-      <div><img src={car} alt={car} /></div>
-      <div></div>
+    <section className="w-full justify-between px-2 md:px-16 py-10 flex flex-col md:flex-row gap-6 lg:gap-12 items-center ">
+      <div className=" w-full md:w-[90%] flex items-center ">
+        <img src={car} alt={car} />
+      </div>
+      <Accordion />
     </section>
-  )
-}
+  );
+};
 
-export default FAQ
+export default FAQ;
---
+++ src/pages/about/MissionData.js
@@ -0,0 +1,21 @@
+const data = [
+  {
+    amount: "7k+",
+    id: "Properties for buy",
+    context: " Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+
+  {
+    amount: "4k+",
+    id: "properties to sell",
+    context: " Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+
+  {
+    amount: "221+",
+    id: "daily completed transaction",
+    context: " Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+];
+
+export default data;
