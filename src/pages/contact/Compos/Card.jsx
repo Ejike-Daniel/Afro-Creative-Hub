cc8dca7135f8e1d8e3acdacf6b6ea04f22bafb2c
Janet Bello <bellojanet022@gmail.com>
2024-05-17 15:27:33 +0100

    made background image responsive

--- src/pages/contact/Compos/Card.jsx
+++ src/pages/contact/Compos/Card.jsx
@@ -3,7 +3,7 @@ import Inform from "./Inform";
 
 const Card = () => {
   return (
-    <div className=" mt-[6rem] md:p-3 md:w-full lg:w-fit flex flex-col md:mx-auto gap-10 rounded-[10px] md:mb-[5rem]  shadow-xl shadow-mountainMist lg:flex-row ">
+    <div className=" mt-[6rem] md:p-10 lg:p-3 md:w-full lg:w-fit flex flex-col md:mx-auto gap-10 rounded-[10px] md:mb-[5rem]  shadow-xl shadow-mountainMist lg:flex-row ">
       <Inform/>
       <FormArea />
     </div>
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -5,28 +5,30 @@ import bottommixed from "../../../src/assets/bgTopmixed.png";
 
 const Contact = () => {
   return (
-
-    
-     
-      
     <Container>
       <div className=" relative">
-       <img src={topMixed} alt="" className=" w-[30rem] absolute top-[-4rem] bottom-[rem] right-0 -z-50 " />
-      
-      
-      <div className=" mx-auto">
-        <h2 className=" text-center text-duckyYellow text-3xl font-bold">Contact Us</h2>
-      <p className=" text-center pb-[3rem]">
-        Any question or remarks? Just write us a message!
-      </p>
-     <Card/>
-    </div>
-    <img src={bottommixed} alt="" className=" w-[30rem] absolute  top-[26rem] left-0 -z-50 " />
-    </div>
-    </Container>
+        <img
+          src={topMixed}
+          alt=""
+          className=" hidden md:flex w-[20rem] lg:w-[30rem] absolute top-[4rem]  lg:top-[-4rem]  right-0 -z-50 "
+        />
 
-    
- 
+        <div className=" mx-auto">
+          <h2 className=" text-center text-duckyYellow text-3xl font-bold">
+            Contact Us
+          </h2>
+          <p className=" text-center pb-[3rem]">
+            Any question or remarks? Just write us a message!
+          </p>
+          <Card />
+        </div>
+        <img
+          src={bottommixed}
+          alt=""
+          className=" hidden md:flex w-[20rem] lg:w-[30rem] absolute md:bottom-[30rem] lg:top-[26rem] left-0 -z-50 "
+        />
+      </div>
+    </Container>
   );
 };
 
