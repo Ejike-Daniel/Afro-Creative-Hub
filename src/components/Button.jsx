7b8d7b98234a7107f59ee574fa8f49867dbeb35c
Elrey009 <daniloejike@gmail.com>
2024-05-23 00:07:00 +0100

    modified files

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 const Button = ({ children, className, onClick }) => {
   return (
     <button
-    onClick={onClick}
-      className={`${className}  text-center font-medium w-56 md:w-[16rem] text-white lg:hover:text-midnight lg:hover:bg-sunriseOrange py-3 rounded-3xl`}
+      onClick={onClick}
+      className={`${className}  text-center font-medium w-56 md:w-[16rem] text-white lg:hover:text-midnight lg:hover:shadow-lg lg:hover:shadow-ragoonGreen py-3 rounded-3xl`}
     >
       {children}
     </button>
